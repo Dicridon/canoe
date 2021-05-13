c857d5a4031733b32d3ecb8286d0569a73839ecf
Dicridon <noahxiong@outlook.com>
2021-05-13 16:12:02 +0800

    versino updated

--- lib/workspace/version.rb
+++ lib/workspace/version.rb
@@ -2,7 +2,7 @@ module Canoe
   class WorkSpace
     def self.version
       puts <<~VER
-           canoe v0.3.2
+           canoe v0.3.2.1
            For features in this version, please visit https://github.com/Dicridon/canoe
            Currently, canoe can do below:
                - project creation
